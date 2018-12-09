b2774eba056249fd7f2bded2770578add102f108
ADMIN <ADMIN@DESKTOP-H2TL9NJ>
2018-12-10 00:20:24 +0700

    add new test data

---
+++ Test Suites/Facebook - Register Multiple User with Email.ts
@@ -0,0 +1,76 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Facebook - Register Multiple User with Email</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>bb0f741e-355f-490b-9a57-21bfb3dcd79d</testSuiteGuid>
+   <testCaseLink>
+      <guid>90ca3403-cee1-4d16-b9c6-3e51b218bb9a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Register Facebook by Email</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>00b5dda5-cec2-4ba1-a227-7d7805e1d7df</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/New User</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>00b5dda5-cec2-4ba1-a227-7d7805e1d7df</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>firstname</value>
+         <variableId>245889f8-d36d-4c9a-b409-4bc40b82d8d7</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>00b5dda5-cec2-4ba1-a227-7d7805e1d7df</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>lastname</value>
+         <variableId>4cfd80cd-501b-4e49-b8b0-31e94a2946da</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>00b5dda5-cec2-4ba1-a227-7d7805e1d7df</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>email</value>
+         <variableId>bda03b3b-f2ba-436e-8643-681a6f064b0a</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>00b5dda5-cec2-4ba1-a227-7d7805e1d7df</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>birthdate</value>
+         <variableId>33ec9547-2743-4d68-97d1-9229c367804a</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>00b5dda5-cec2-4ba1-a227-7d7805e1d7df</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>birthmonth</value>
+         <variableId>d9325225-badc-45fe-b124-5aa36a46c60b</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>00b5dda5-cec2-4ba1-a227-7d7805e1d7df</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>birthyear</value>
+         <variableId>161bbee4-b4b8-4712-a8ef-88b14bd3e880</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>00b5dda5-cec2-4ba1-a227-7d7805e1d7df</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>gender</value>
+         <variableId>9f55a579-b289-49e5-a29c-dbbe2b3a8daf</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>00b5dda5-cec2-4ba1-a227-7d7805e1d7df</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>7f5004fe-eee3-4628-965b-ba375bdbbf43</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
